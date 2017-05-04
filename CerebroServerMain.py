1887880bcf99c7f43eba6b5767fddefc6629898e
Nihat <nihat.atay.93@gmail.com>
2017-05-05 02:46:26 +0300

    hatalı pushlama. Düzelttim.d

--- CerebroServerMain.py
+++ CerebroServerMain.py
@@ -1,18 +1,15 @@
 import os
 from flask import Flask, request, jsonify, abort
 from werkzeug.utils import secure_filename
-
-import urllib.parse
-
 app = Flask(__name__)
 
 
-#APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-#UPLOAD_FOLDER = os.path.join(APP_ROOT, './uploads')
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-#ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, './uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #book stubs
 stub_books = [
     {
@@ -68,22 +65,22 @@ def search_movie_by_id(id):
 def get_books():
     return jsonify({'books':stub_books})
 
-#def allowed_file(filename):
-#    return '.' in filename and \
-#           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-#@app.route('/media/api/v1.0/movies/s', methods = ['POST'])
-#def search_movie_by_image():
-#    if request.method == 'POST':
-#        if 'file' not in request.files:
-            # return 'no file part'
-        # file = request.files['file']
-        # if file.filename == '':
-            # return 'no filename'
-        # if file and allowed_file(file.filename):
-            # filename = secure_filename(file.filename)
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # return 'file uploaded'
+@app.route('/media/api/v1.0/movies/s', methods = ['POST'])
+def search_movie_by_image():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'no file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'no filename'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'file uploaded'
 
 if __name__ == '__main__':
-   app.run(debug = True)
+app.run(debug = True)
